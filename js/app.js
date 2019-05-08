40fbfeb3e84da4f9f921b1bac0a7d74fee432789
sanjeevi <sanjeevi.vemuri@gmail.com>
2019-05-08 05:44:54 -0700

    Lab02

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var likesgameofthrones = prompt('Do you like Game of Thrones?');
+
+likesgameofthrones =likesgameofthrones.toLowerCase();
+
+if (likesgameofthrones ==="yes" || likesgameofthrones ==='y') {
+  alert ( 'me too!');
+} else {
+ alert ( 'How can you not like it ?');
+}
+
+
+ 
+
+ var youenjoytheclass = prompt('Do you enjoy the class?');
+
+ youenjoytheclass =youenjoytheclass.toLowerCase();
+ 
+ if (youenjoytheclass ==="yes" || youenjoytheclass ==='y') {
+  alert ( 'Thats Great!');
+} else { 
+  alert ( 'How can we make it better ?');
+}
+
+
+
+  var knowjavascript = prompt('Do you know javascript?');
+
+ knowjavascript = knowjavascript.toUpperCase();
+ 
+ if (knowjavascript ==="YES" || youenjoytheclass ==='Y'){
+  alert ( 'Thats a good start');
+}else {
+  alert ( 'Well you have no choice BUT to learn ?');
+}
